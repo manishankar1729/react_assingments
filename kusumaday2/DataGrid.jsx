c003e06c2ddfb39b618c32cd6b0dcd081587138f
Seelam-Kusuma <seelamkusumasri555@gmail.com>
2025-03-26 10:09:35 +0530

    Add files via upload

---
+++ kusumaday2/DataGrid.jsx
@@ -0,0 +1,148 @@
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import { DataGrid } from '@mui/x-data-grid';
+
+
+// export default class DataGridDemo extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             rows: [
+//                 { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+//                 { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+//                 { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+//                 { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+//                 { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//                 { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//                 { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//                 { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//                 { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+//             ],
+//             columns: [
+//                 { field: 'id', headerName: 'ID', width: 90 },
+//                 {
+//                     field: 'firstName',
+//                     headerName: 'First name',
+//                     width: 150,
+//                     editable: true,
+//                 },
+//                 {
+//                     field: 'lastName',
+//                     headerName: 'Last name',
+//                     width: 150,
+//                     editable: true,
+//                 },
+//                 {
+//                     field: 'age',
+//                     headerName: 'Age',
+//                     type: 'number',
+//                     width: 110,
+//                     editable: true,
+//                 },
+//                 {
+//                     field: 'fullName',
+//                     headerName: 'Full name',
+//                     description: 'This column has a value getter and is not sortable.',
+//                     sortable: false,
+//                     width: 160,
+//                     valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+//                 },
+//             ]
+            
+
+//         }
+//     }
+//     render(){
+//         const {rows, columns} = this.state;
+//         const DataGridDemo = ({ users }) => {
+//         return (
+//             <Box sx={{ height: 400, width: '100%' }}>
+//                 <DataGrid
+//                     rows={rows}
+//                     columns={columns}
+//                     initialState={{
+//                         pagination: {
+//                             paginationModel:{
+//                             pageSize: 5,}
+//                         },
+//                     }}
+//                     pageSizeOptions={[5]}
+//                     checkboxSelection
+//                     disableRowSelectionOnClick
+//                 />
+//             </Box>
+//         );
+//     };
+//     }
+
+// }
+
+import React, { Component } from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
+export default class DataGridDemo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            rows: [],
+            columns: [],
+        };
+    }
+
+    async componentDidMount() {
+        let response = await fetch("https://jsonplaceholder.typicode.com/users");
+        let data = await response.json();
+
+        // Flatten the data structure
+        const flattenedData = data.map((item) => ({
+            id: item.id,
+            name: item.name,
+            username: item.username,
+            email: item.email,
+            phone: item.phone,
+            website: item.website,
+            'address.street': item.address?.street || '',
+            'address.suite': item.address?.suite || '',
+            'address.city': item.address?.city || '',
+            'address.zipcode': item.address?.zipcode || '',
+            'geo.lat': item.address?.geo?.lat || '',
+            'geo.lng': item.address?.geo?.lng || '',
+            'company.name': item.company?.name || '',
+            'company.catchPhrase': item.company?.catchPhrase || '',
+            'company.bs': item.company?.bs || '',
+        }));
+
+        
+        const columns = Object.keys(flattenedData[0]).map((key) => ({
+            field: key,
+            headerName: key.toUpperCase(),
+            width: 150,
+            editable: true,
+        }));
+
+        this.setState({ rows: flattenedData, columns });
+    }
+
+    render() {
+        const { rows, columns } = this.state;
+        return (
+            <Box sx={{ height: 600, width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 5,
+                            },
+                        },
+                    }}
+                    pageSizeOptions={[5, 10, 20]}
+                    checkboxSelection
+                    disableRowSelectionOnClick
+                />
+            </Box>
+        );
+    }
+}
